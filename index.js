35bd0e3b96341333a59e75fa68bc9208b9440ef3
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-06-04 14:52:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    //res.header("Access-Control-Allow-Origin", '*');
+    //res.header("Access-Control-Allow-Credentials", true);
+    //res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    //res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, content-type, application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     next();
 });
 
@@ -21,7 +24,7 @@ app.get("/players", (req, res) => {
 
 app.get("/match", (req, res) => {
     let a = importData;
-    res.setHeader()
+    res.setHeader("Content-Type", "text/html");
     res.send({ body: importData })
 });
 

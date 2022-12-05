25cfb081015d211802e32697aacde80dc91d5d91
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-12-05 22:17:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-require('dotenv').config()
+const bodyParser = require("body-parser");
+require("dotenv").config()
 let port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
     next();
 });
 

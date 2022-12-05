0a42289d49a415e5233221986c588c4eed109b2e
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-12-05 23:05:58 +0100

    cors

--- server.js
+++ server.js
@@ -2,25 +2,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 require("dotenv").config()
+const cors = require('cors')
 let port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
+    res.header("Access-Control-Allow-Origin", "https://my-endpoints.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
     next();
 });
 

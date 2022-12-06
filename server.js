d4aff53835b98bbdfd3a593fb1d0c6859a9aa250
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2022-12-06 08:25:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://valeriodellefave.github.io");
-    res.header("Access-Control-Allow-Origin", "https://my-endpoints.herokuapp.com");
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    
+    res.setHeader("Access-Control-Allow-Origin", "https://valeriodellefave.github.io");
+    res.setHeader("Access-Control-Allow-Origin", "https://my-endpoints.herokuapp.com");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+
     next();
 });
 

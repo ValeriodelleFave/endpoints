48f8795d379582711de2fbb8f2c49feb5c7c6ad1
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-12-07 22:37:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,11 @@ let port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: "https://valeriodellefave.github.io",
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
-/**
- * 
- 
+
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://valeriodellefave.github.io");
-    res.setHeader("Access-Control-Allow-Origin", "https://my-endpoints.herokuapp.com");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
-*/
 
 app.use("/", require("./routes/index"));
 

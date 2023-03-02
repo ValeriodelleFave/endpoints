78f2b1462d81e2bdbdb691e21e75055c696cbb97
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2023-03-02 22:36:42 +0100

    fix vari

--- mongodb-connection.js
+++ mongodb-connection.js
@@ -1,27 +1,35 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mycluster.ed48vnx.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
 const db = client.db("budget_management");
 
 const MongoAgent = {
-    connect: function connect() {
-        client.connect().finally(() => {
-            console.log("Connessione Mongo");
-        });
-    },
-    insertOne: async function insertOne(data) {
-      try {
-        const budget = db.collection("budget");
-        const result = await budget.insertOne(data);
-      } finally {
-        console.log("Operazione di inserimento inviata");
-      }
-    },
-    disconnect: function disconnect() {
-        client.close().finally(() => {
-            console.log("Disconnessione Mongo");
-        });
+  insertOne: async function insertOne(data) {
+    try {
+      await client.connect();
+      const response = await db.collection("budget").insertOne(data);
+      console.log(`Il documento con id ${response.insertedId} è stato inserito correttamente.`);
+    } catch (err) {
+      console.log("Operazione fallita. ", err);
+    } finally {
+      await client.close();
     }
+  },
+  getAll: async function getAll() {
+    try {
+      const budget = db.collection("budget");
+      return await budget.find({});
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 
 module.exports = MongoAgent;
--- routes/budget-management.js
+++ routes/budget-management.js
@@ -1,25 +1,18 @@
 const router = require('express').Router();
 const mongo = require('../mongodb-connection')
 
-router.get("/", (req, res) => {
-    res.json("Hello World");
-});
-
-router.post("/", (req, res) => {
-    mongo.insertOne(req.body)
+router.post("/", async (req, res) => {
+    const response = await mongo.insertOne(req.body)
     res.json("Inserimento DB");
 });
 
-router.post("/connect", (req, res) => {
-    mongo.connect();
-    res.json("Connessione DB")
+router.get("/getAll", async (req, res) => {
+    const re = await mongo.getAll()
+    // re.forEach
+    debugger
+    res.json(re);
 });
 
-router.post("/disconnect", (req, res) => {
-    mongo.disconnect();
-    res.json("Disconnessione DB")
-});
-
-router.use("/budget-management", router)
+router.use("/budget-management", router);
 
 module.exports = router;

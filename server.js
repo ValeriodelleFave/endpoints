38d3645169e24943d4091b89de059e8e44e786b7
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2023-03-29 19:02:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://valeriodellefave.dev");
+    res.setHeader("Access-Control-Allow-Origin", "https://www.valeriodellefave.dev");
     res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
     res.setHeader('Access-Control-Allow-Credentials', true);

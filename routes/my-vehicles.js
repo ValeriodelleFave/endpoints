96335a7d52b29fc8e7048a1c677d50a8488b0d17
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2024-04-28 21:27:31 +0200

    Cose belle

--- routes/my-vehicles.js
+++ routes/my-vehicles.js
@@ -1,44 +1,21 @@
 const router = require('express').Router();
 
 router.get("/get-vehicles", (req, res) => {
-    res.json(
-        [{
-            id: 1,
-            targa: "EY001KW",
-            marca: "Alfa Romeo",
-            modello: "Giulietta",
-            specifica: "Sprint",
-            km: 134300
-        }]
-    );
+    res.json(require("./../data/cars.json"));
 });
 
 router.get("/get-vehicle-notifications/:id", (req, res) => {
-    res.json(
-        {
-            id: 1,
-            targa: "EY001KW",
-            marca: "Alfa Romeo",
-            modello: "Giulietta",
-            specifica: "Sprint",
-            km: 134300,
-            notifications: [
-                {
-                    id: 1,
-                    titolo: "Tagliando",
-                    scadenza: 30000,
-                    ultimaRegistrazione: 130000
-                },
-                {
-                    id: 2,
-                    titolo: "Cambio olio motore",
-                    scadenza: 10000,
-                    ultimaRegistrazione: 12300
-                },
-            ]
-        }
-    );
+    const car = require("./../data/cars.json").find(element => element.id === Number(req.params.id))
+    res.json(car);
 });
+
+router.delete("/:carId/cancel-notification/:id", (req, res) => {
+    res.json(req.params)
+})
+
+router.put("/update-km", (req, res) => {
+    res.json(req.body)
+})
 
 
 router.use("/my-vehicles", router)
